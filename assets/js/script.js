dcec6435db64172545d9f1ebc7493aea5c33e442
BahraamK <>
2024-01-08 08:23:48 +0000

    adjust keywords to fix result not showing

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,28 +8,29 @@ const computerScore = document.getElementById("computer-score");
 
 function startGame(playerOption) {
     const computerOption = options[Math.floor(Math.random() * 3)];
-    let result = "";
+    let outcome = "";
     if (playerOption === computerOption) {
-        result = "You Tied";
+        outcome = "You Tied";
     }
     else {
         switch (playerOption) {
             case "rock":
-                result = (computerOption === "scissors") ? "You Won" : "You Lose";
+                outcome = (computerOption === "scissors") ? "You Won" : "You Lose";
                 break;
             case "paper":
-                result = (computerOption === "rock") ? "You Won" : "You Lose";
+                outcome = (computerOption === "rock") ? "You Won" : "You Lose";
                 break;
             case "scissors":
-                result = (computerOption === "paper") ? "You Won" : "You Lose";
+                outcome = (computerOption === "paper") ? "You Won" : "You Lose";
                 break;
         }
 
     }
     computerDecision.textContent = `Computer: ${computerOption}`;
     playerDecision.textContent = `Player: ${playerOption}`;
-    result.textContent = result;
+    result.textContent = outcome;
 }
+
 
 
 
