3d146488385ea7972c493e41c1dec33dedfdcd1b
BahraamK <>
2024-01-08 08:07:08 +0000

    declare main constants

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+const playerDecision = document.getElementById("player-decision");
+const computerDecision = document.getElementById("computer-decision");
+const options = ["rock", "paper", "scissors"];
+const result = document.getElementById("result");
+const playerScore= document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+
+
+
+
