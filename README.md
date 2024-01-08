066a1c857a0b13ae709b7ea6a0cdccc9789b4e09
BahraamK <>
2024-01-08 10:05:16 +0000

    add overview and user story

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# RockPaperScissors
 
+RockPaperScissors is a site that hopes to encourage curious people to take a break from their work and have some fun playing a game against a computer which demonstrates the use of javascript. RockPaperScissors game built using primarly javascript which allow users to endlessly play games of rock,paper,scissors against a computer.
+
+## User Experience
+
+### User Stories
+
+1.As a user who is bored and want to play a game
+2.I expect to find a simple and easy to learn game
+3.As someone who is interested in the demonstration of javascript
+4.I expect to play a game built using javascript
+5.As a user who wants to learn the rules to the game
+6.I expect to find the rules of the game within the webpage
+7.As a user who is looking to track how i have been doing in the game
+8.I expect to see a score tracker
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,26 @@
 
 const playerDecision = document.getElementById("player-decision");
 const computerDecision = document.getElementById("computer-decision");
+//Array of options 
 const options = ["rock", "paper", "scissors"];
 const result = document.getElementById("result");
 const playerScore= document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
-
+//Declared variables to begin with score value 0
 let playerWins=0;
 let computerWins=0;
-
+//Chooses randomly amung the 3 options
 function startGame(playerOption) {
     const computerOption = options[Math.floor(Math.random() * 3)];
     let outcome = "";
+    //Outcome when user and computer decision are the same
     if (playerOption === computerOption) {
         outcome = "You Tied";
     }
+    /*
+    All possible outcomes when user and computer
+    have different decisions
+    */
     else {
         switch (playerOption) {
             case "rock":
@@ -29,10 +35,14 @@ function startGame(playerOption) {
         }
 
     }
+    //Displays decisions on screen
     computerDecision.textContent = `Computer: ${computerOption}`;
     playerDecision.textContent = `Player: ${playerOption}`;
     result.textContent = outcome;
-    
+    /*
+    Incrementally increases scores
+    based on different outcomes
+    */
     switch (outcome) {
         case "You Won":
             playerWins++;
